1c4ac2d5ca8f07a7fe0ecd25a9d5b50069e192e5
juanjopadilla21 <juanjopm1410@gmail.com>
2026-05-12 23:07:31 -0500

    implementar bucle for para notas

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ while siguente == "si":
     nombre = input("Ingrese el nombre del estudiante: ")
 
     suma = 0
+ # Bucle for para las 5 notas
+    for i in range(1, 6):
+
+        # Mensaje para contador
+        nota = float(input("Ingrese nota " + str(i) + ": "))
+
+        # Validar que la nota esté entre 1.0 y 5.0
+        while nota < 1.0 or nota > 5.0:
+            print("Error. La nota debe estar entre 1.0 y 5.0")
+            nota = float(input("Ingrese nuevamente la nota " + str(i) + ": "))
+
+        suma = suma + nota
+
+    # Cálculo del promedio
+    promedio = suma / 5
+
